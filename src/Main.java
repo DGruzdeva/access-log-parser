5bbb963ae246620401818e47ec749e3dac5481ac
Darya Gruzdeva <dashagruzd@gmail.com>
2024-10-07 14:33:00 +0300

    Добавился код для вычислений

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in) .nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in) .nextInt();
+        double quotient1 = (double) firstNumber / secondNumber;
+        double quotient2 = (double) firstNumber * secondNumber;
+        int quotient3 = firstNumber + secondNumber;
+        int quotient4 = firstNumber - secondNumber;
+        System.out.println("Частное: " + quotient1);
+        System.out.println("Частное: " + quotient2);
+        System.out.println(quotient3);
+        System.out.println(quotient4);
+
     }
 }
