7ed7635e56d5814a668872652c6f7666cb3097ee
Darya Gruzdeva <dashagruzd@gmail.com>
2025-11-17 16:32:36 +0300

    Add 2 number experiments

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in) .nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in) .nextInt();
+        int addition = (int) firstNumber + secondNumber;
+        int subtraction = (int) firstNumber - secondNumber;
+        int multiplication = (int) firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сложение:" + addition);
+        System.out.println("Вычитание:" + subtraction);
+        System.out.println("Деление:" + quotient);
+        System.out.println("Умножение:" + multiplication);
         }
 }
